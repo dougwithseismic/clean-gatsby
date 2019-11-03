2aec0483e6bbefea0469d95306f3a426b7ccdda1
Doug <doug@withseismic.com>
2019-11-03 11:45:40 +0100

    Initial Push

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,10 @@
 // We're wrapping everything in our Providers so we can consume them on all pages
 
 import React, { Fragment, useContext } from 'react'
-import { ModalProvider, ModalContext } from './src/context/ModalContext'
-import Modal from './src/components/common/Modal'
-import OfferModal from './src/components/common/Modal/OfferModal'
-
 import './global.css'
 
 export const wrapRootElement = ({ element }) => (
-  <ModalProvider>
-    <Modal>
-      <OfferModal />
-    </Modal>{' '}
+  <div className="content">
     {element}
-  </ModalProvider>
+  </div>
 )
